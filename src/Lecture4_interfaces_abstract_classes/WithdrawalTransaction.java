11b30d7b9f86958310659df5d879eff386a23070
fozzah <abdallahx002@gmail.com>
2024-11-29 18:07:27 +0300

    Adding Assignment 1

--- src/Lecture4_interfaces_abstract_classes/WithdrawalTransaction.java
+++ src/Lecture4_interfaces_abstract_classes/WithdrawalTransaction.java
@@ -5,41 +5,32 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Calendar;
 
 public class WithdrawalTransaction extends BaseTransaction {
-    public WithdrawalTransaction(int amount, @NotNull Calendar date) {
-        super(amount, date);
+    private boolean reversed = false;
+
+    public WithdrawalTransaction(double amount, Calendar date, String transactionID) {
+        super(amount, date, transactionID);
     }
 
-    private boolean checkDepositAmount(int amt) {
-        if (amt < 0) {
-            return false;
+    @Override
+    public void apply(BankAccount ba) {
+        if (ba.getBalance() >= getAmount()) {
+            ba.withdraw(getAmount());
+            System.out.println("Withdrawal of $" + getAmount() + " applied.");
         } else {
+            System.out.println("Insufficient funds for withdrawal.");
+        }
+    }
+    /*
+   Assignment 1 Q3: Write the Reverse method - a method unique to the WithdrawalTransaction Class
+    */
+    public boolean reverse(BankAccount ba) {
+        if (!reversed) {
+            ba.deposit(getAmount());
+            reversed = true;
+            System.out.println("Withdrawal reversed.");
             return true;
         }
+        return false;
     }
-
-    // Method to reverse the transaction
-    public boolean reverse() {
-        return true;
-    } // return true if reversal was successful
-
-    // Method to print a transaction receipt or details
-    public void printTransactionDetails() {
-        System.out.println("Deposit Trasaction: " + this.toString());
-    }
-
-    /*
-    Oportunity for assignment: implementing different form of withdrawal
-     */
-    public void apply(BankAccount ba) {
-        double curr_balance = ba.getBalance();
-        if (curr_balance > getAmount()) {
-            double new_balance = curr_balance - getAmount();
-            ba.setBalance(new_balance);
-        }
-    }
-
-    /*
-    Assignment 1 Q3: Write the Reverse method - a method unique to the WithdrawalTransaction Class
-     */
 }
 
